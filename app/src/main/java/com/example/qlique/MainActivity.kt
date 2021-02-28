503ef841a117957d460fd6c2ac4b1c35dd47e0dd
ASUS <ASUS@LAPTOP-3N47BETR>
2021-02-28 19:07:54 +0200

    created the login activities: LoginActivity.kt SignupActivity.kt ForgotPasswordActivity.kt UpdatePassword.kt and the main activity. no default activity was made yet.

---
+++ app/src/main/java/com/example/qlique/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.qlique
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var logoutBtn: Button
+    private lateinit var updatePass: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser == null){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            Toast.makeText(this, "Already logged in", Toast.LENGTH_LONG).show()
+        }
+
+        setContentView(R.layout.activity_main)
+
+        logoutBtn = findViewById(R.id.logout_btn)
+        updatePass = findViewById(R.id.update_pass_btn)
+
+        logoutBtn.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        updatePass.setOnClickListener{
+            val intent = Intent(this, UpdatePassword::class.java)
+            startActivity(intent)
+        }
+    }
+}
