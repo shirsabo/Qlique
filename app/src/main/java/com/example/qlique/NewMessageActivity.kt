2b1892e7ec874c3e4d4a28580dd4b01119401181
shirs <shirs@DESKTOP-QR9AL08>
2021-03-07 23:58:53 +0200

    added chat activities

--- app/src/main/java/com/example/qlique/NewMessageActivity.kt
+++ app/src/main/java/com/example/qlique/NewMessageActivity.kt
@@ -1,25 +1,24 @@
 package com.example.qlique
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_new_message.*
-import kotlinx.android.synthetic.main.activity_profile_page.*
+
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
         supportActionBar?.title="Select User"
-        val adapter =GroupAdapter<com.xwray.groupie.GroupieViewHolder>()
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
+        val adapter = GroupAdapter<com.xwray.groupie.GroupieViewHolder>()
+        fetchFriends()
         newMessageRecycle.adapter= adapter
         adapter.setOnItemClickListener{item,view->
             val intent = Intent (view.context,chatLogActivity::class.java)
@@ -28,6 +27,46 @@ class NewMessageActivity : AppCompatActivity() {
         }
     }
 }
+
+private fun fetchFriends(){
+    val mFirebaseInstance= FirebaseDatabase.getInstance()
+    val mFirebaseDatabase=mFirebaseInstance!!.getReference("users")
+    val userId=FirebaseAuth.getInstance().uid
+    mFirebaseDatabase!!.child(userId!!).addValueEventListener(object: ValueEventListener{
+        override fun onDataChange(dataSnapshot: DataSnapshot){
+            val user=dataSnapshot.getValue(User::class.java)
+            if(user!=null){
+                for (friend in user.getFriends()){ val q: Query = mFirebaseDatabase.child("users").orderByChild("email")
+                        .equalTo(friend)
+                    if(q!=null){
+                        q.addValueEventListener(object : ValueEventListener {
+                            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                for (data in dataSnapshot.children) {
+                                    val models: User? = data.getValue(User::class.java)
+                                    Log.d("tag",models?.getFirstName())
+                                }
+                            }
+
+                            override fun onCancelled(databaseError: DatabaseError) {}
+                        })
+
+
+                    }
+
+                }
+            }
+
+
+
+        }
+        override fun onCancelled(error: DatabaseError){
+            //Failed to read value
+        }
+    })
+
+}
+
+
 class UserItem: Item<com.xwray.groupie.GroupieViewHolder>(){
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         // later on..
--- app/src/main/java/com/example/qlique/User.java
+++ app/src/main/java/com/example/qlique/User.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class User {
     public String firstName, lastName, email, city, gender;
     public List<String> friends;
-
+    public List<String> hobbies;
     public User(String firstName, String lastName, String city, String email, String gender) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -15,7 +15,25 @@ public class User {
         this.gender = gender;
         this.friends = new ArrayList<>();
     }
-
+    public List<String>getFriends() {
+        if (this.friends!=null){
+            return this.friends;
+        }
+        return null;
+    }
+    public List<String>getHobbiess() {
+        if (this.hobbies!=null){
+            return this.hobbies;
+        }
+        return null;
+    }
+    public String getFirstName() {
+        if (this.hobbies!=null){
+            return this.firstName;
+        }
+        return null;
+    }
     public User() {
     }
+
 }
