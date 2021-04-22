c59a7f9b6d71c55a472b0508f5a7fbfbab1357d8
shirs <shirsabo100@gmail.com>
2021-04-22 18:29:24 +0300

    fixed bug issue.

--- app/src/main/java/com/example/qlique/Profile/ProfilePage.java
+++ app/src/main/java/com/example/qlique/Profile/ProfilePage.java
@@ -3,12 +3,15 @@ package com.example.qlique.Profile;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,8 +25,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.Objects;
@@ -34,6 +39,29 @@ public class ProfilePage extends AppCompatActivity{
     private User user;
     private String userIdProfile;
     ArrayAdapter<String> adapter;
+    void setProfile(){
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference ref = database.getReference("users/"+FirebaseAuth.getInstance().getCurrentUser().getUid());
+// Attach a listener to read the data at our posts reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                String  url = user.url;
+                if(url==null){
+                    return;
+                }
+                ImageView profile = findViewById(R.id.ProfileCircularImage);
+                Picasso.get().load(user.url).into(profile);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,9 +77,7 @@ public class ProfilePage extends AppCompatActivity{
         Button chat = findViewById(R.id.envelop);
         com.mikhaellopez.circularimageview.CircularImageView profilePic = findViewById(R.id.ProfileCircularImage);
         User curUser = SignupActivity.Companion.getCurrentUser();
-        if (curUser !=null&& curUser.url!=null){
-            Picasso.get().load(curUser.url).into(profilePic);
-        }
+        setProfile();
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
         userIdProfile = getIntent().getStringExtra("EXTRA_SESSION_ID");
         assert userIdProfile != null;
