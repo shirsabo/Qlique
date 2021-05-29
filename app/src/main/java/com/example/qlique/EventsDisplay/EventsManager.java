663f228bec7c98fe887f3636b2b281074918cd24
shirs <shirsabo100@gmail.com>
2021-05-29 21:45:18 +0300

    added dialog for capacity, and join dialog

--- app/src/main/java/com/example/qlique/EventsDisplay/EventsManager.java
+++ app/src/main/java/com/example/qlique/EventsDisplay/EventsManager.java
@@ -31,6 +31,8 @@ import com.example.qlique.R;
 import com.google.android.material.navigation.NavigationView;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Semaphore;
 
 import com.example.qlique.CreateEvent.Event;
@@ -41,18 +43,27 @@ public class EventsManager extends AppCompatActivity implements NavigationView.O
     RecyclerView recyclerView;
     private GroupAdapter<GroupieViewHolder> groupieAdapter =
             new GroupAdapter<com.xwray.groupie.GroupieViewHolder>();
+    List<String> eventsLists = new ArrayList();
     private void addEvents(String eventIn){
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("posts/"+eventIn);
+        if(eventsLists.contains(eventIn)){
+            return;
+        }
         // Attach a listener to read the data at our posts reference
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Event event = dataSnapshot.getValue(Event.class);
                 event.uid = dataSnapshot.child("uid").getValue().toString();
+                event.description =dataSnapshot.child("description").getValue().toString();
+                event.date = dataSnapshot.child("date").getValue().toString();
+                event.header = dataSnapshot.child("header").getValue().toString();
+                event.photoUrl = dataSnapshot.child("photoUrl").getValue().toString();
                 /** TO DO: Check that the event has not yet occurred **/
                 if(event==null){return;}
                 groupieAdapter.add(new EventItem(event));
+                eventsLists.add(eventIn);
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -63,7 +74,6 @@ public class EventsManager extends AppCompatActivity implements NavigationView.O
 
     }
     private void fetchEvents(){
-        final Semaphore semaphore = new Semaphore(0);
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("users/"+ FirebaseAuth.getInstance().getCurrentUser().getUid());
         // Attach a listener to read the data at our posts reference
@@ -122,6 +132,10 @@ public class EventsManager extends AppCompatActivity implements NavigationView.O
                 i.putExtra("event", (Serializable) event);
                 v.getContext().startActivity(i);
             });
+            viewHolder.itemView.findViewById(R.id.leave_btn_custom).setOnClickListener(v -> {
+                handleExitEvent(event,this);
+
+            });
             final FirebaseDatabase database = FirebaseDatabase.getInstance();
             if(event ==null ||event.uid==null){
                 return;
@@ -129,10 +143,14 @@ public class EventsManager extends AppCompatActivity implements NavigationView.O
             DatabaseReference ref = database.getReference("users/"+event.uid);
             // Attach a listener to read the data at our posts reference
             ref.addValueEventListener(new ValueEventListener() {
+               // @SuppressLint("SetTextI18n")
                 @SuppressLint("SetTextI18n")
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     User user = dataSnapshot.getValue(User.class);
+                    if(user==null){
+                        return;
+                    }
                     String  uri = event.photoUrl;
                     ImageView targetImageView = viewHolder.itemView.findViewById(R.id.cardImage);
                     ImageView targetAuthorImageView = viewHolder.itemView.findViewById(R.id.photo_event_new);
@@ -143,9 +161,14 @@ public class EventsManager extends AppCompatActivity implements NavigationView.O
                     targetImageView.setColorFilter(Color.argb(155, 0, 0, 0), PorterDuff.Mode.SRC_ATOP);
                     TextView targetAuthor =viewHolder.itemView.findViewById(R.id.member_username);
                     targetAuthor.setText(user.firstName+" "+user.lastName);
-                    targetTextView.setText(event.description);
-                    TextView date = viewHolder.itemView.findViewById(R.id.date);
-                    date.setText(event.date);
+                    if(event.description!=null){
+                        targetTextView.setText(event.header);
+                    }
+                    if(event.date!=null){
+                        TextView date = viewHolder.itemView.findViewById(R.id.date);
+                        date.setText(event.date);
+                    }
+
                 }
 
                 @Override
@@ -160,6 +183,85 @@ public class EventsManager extends AppCompatActivity implements NavigationView.O
         public int getLayout() {
             return R.layout.event_custom_in_events_display;
         }
+    }
+
+    private void handleExitEvent(Event event,EventItem eventItem) {
+        if(event==null){
+            return;
+        }
+        else{ //delete eventUid from events in current user's events.
+            String eventUid = new String(event.eventUid);
+            ExitEventFromCurUser(event.eventUid);
+            deleteUserFromEventsMembers(eventUid,eventItem);
+        }
+    }
+
+    private void ExitEventFromCurUser(String eventUid) {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("users/"+ FirebaseAuth.getInstance().getCurrentUser().getUid());
+        // Attach a listener to read the data at our posts reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                user.uid = dataSnapshot.child("uid").getValue().toString();
+                user.url=dataSnapshot.child("url").getValue().toString();
+                user.lastName=dataSnapshot.child("lastName").getValue().toString();
+                if(eventUid==null){
+                    return;
+                }
+                assert user != null;
+                if(user.events==null){
+                    return;
+                }
+                user.events.remove(eventUid);//remove event's uid
+                ref.setValue(user);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+        });
+    }
+    private void deleteUserFromEventsMembers(String eventUid,EventItem eventItem){
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("posts/"+eventUid);
+        // Attach a listener to read the data at our posts reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Event event = dataSnapshot.getValue(Event.class);
+                if(event==null){
+                    return;
+                }
+                event.uid = dataSnapshot.child("uid").getValue().toString();
+                event.photoUrl=dataSnapshot.child("photoUrl").getValue().toString();
+                event.description=dataSnapshot.child("description").getValue().toString();
+                if(event.members!=null){
+                    event.members.remove(FirebaseAuth.getInstance().getCurrentUser().getUid());//remove event's uid
+                }
+                ref.setValue(event);
+                /*
+                if(groupieAdapter.getItemCount()>=1){
+                    return;
+                }
+                groupieAdapter.remove(eventItem);
+                */
+                finish();
+                startActivity(getIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
     }
 }
 
